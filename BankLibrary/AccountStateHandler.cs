885774cbde10ff1aca7fc076c0a157059b47cb7f
Stanislav <alcestoneone@gmail.com>
2018-05-29 19:41:06 +0300

    The specification of AccountStateHandler delegate created

---
+++ BankLibrary/AccountStateHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public delegate void AccountStateHandler(object sender, AccountEventArgs e);
+    public class AccountEventArgs
+    {
+        public string Message { get; private set; }//message
+        public decimal Value { get; private set; }//value of transaction
+        public AccountEventArgs(string _mes,decimal _val)
+        {
+            Message = _mes;
+            Value = _val;
+        }
+    }
+}
